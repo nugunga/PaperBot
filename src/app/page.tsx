fd98e1b4292b2027ddb14d967f80da2254364a9c
remin <sms06841@gmail.com>
2024-01-22 18:34:05 +0900

    서버 구축

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import Image from "next/image";
 import styles from "./page.module.css";
@@ -6,20 +6,9 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <>
-     <main className={'유찬이불쌍해'}>
-      <h1>꼬우세요?</h1>
-      <p>
-        명재야 쿠크기믹좀 잘해봐
-      </p>
-    </main>
-    <style jsx>{`
-      #abc{
-        font-size:" 64px;
-      color: red;
-    }
-  `}  
-    </style>
+      <main className={"bitroom"}>
+        <h1>PaperBot</h1>
+      </main>
     </>
-   
   );
 }
