8ffd95bec080675196d330c59d0728b97bb33ea9
PhantomWorker <jat05026@gmail.com>
2024-02-25 15:09:24 +0900

    Update

--- discord-client/database/query/user.ts
+++ discord-client/database/query/user.ts
@@ -1,37 +1,54 @@
 import { User } from "@prisma/client";
 import { prisma } from "..";
 
-async function create(discordID: string, accessToken: string, refreshToken: string) : Promise<User> {
-    const user = prisma.user.create({
-        data:{
-            discordID: discordID,
-            accessToken: accessToken,
-            refreshToken: refreshToken
-        }
-    })
-    return user;
+async function create(
+  id: string,
+  accessToken: string,
+  refreshToken: string
+): Promise<User> {
+  const user = prisma.user.create({
+    data: {
+      id: id,
+      accessToken: accessToken,
+      refreshToken: refreshToken,
+    },
+  });
+  return user;
 }
 
-
-async function update(discordID: string, accessToken: string, refreshToken: string) : Promise<User> {
-    const user = prisma.user.update({
-        where:{ discordID: discordID },
-        data:{
-            accessToken: accessToken,
-            refreshToken: refreshToken
-        }
-    })
-    return user;
+async function update(
+  id: string,
+  accessToken: string,
+  refreshToken: string
+): Promise<User> {
+  const user = prisma.user.update({
+    where: { id: id },
+    data: {
+      accessToken: accessToken,
+      refreshToken: refreshToken,
+    },
+  });
+  return user;
 }
 
-async function findUnique(discordID:string): Promise<User | null> {
-    const user = await prisma.user.findUnique({
-        where:{
-            discordID: discordID
-        }
-    });
+async function findUnique(id: string): Promise<User | null> {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: id,
+    },
+  });
 
-    return user;
+  return user;
 }
 
-export default { create, update, findUnique }
+async function findMany(id: string): Promise<User[] | null> {
+  const user = await prisma.user.findMany({
+    where: {
+      id: id,
+    },
+  });
+
+  return user;
+}
+
+export default { create, update, findUnique, findMany };
--- discord-client/system/redirects.ts
+++ discord-client/system/redirects.ts
@@ -1,50 +1,56 @@
 import passport from "passport";
 import { Profile, Strategy } from "passport-discord";
-import { VerifyCallback } from 'passport-oauth2'
+import { VerifyCallback } from "passport-oauth2";
 import db from "../database";
 
 passport.serializeUser((user: any, done) => {
-    return done(null, user.id);
+  return done(null, user.id);
 });
 
-passport.deserializeUser(async (id:string, done) => {
-    console.log(id);
-    try{
-        const user = await db.user.findUnique(id);
-        console.log(id);
-        return user ? done(null, user) : done(null, null);
-    }
-    catch (err) {
-        console.log(err);
-        return done(err, null);
-    }
+passport.deserializeUser(async (id: string, done) => {
+  try {
+    const user = await db.user.findUnique(id);
+    return user ? done(null, user) : done(null, null);
+  } catch (err) {
+    console.log(err);
+    return done(err, null);
+  }
 });
 
 passport.use(
-    new Strategy(
+  new Strategy(
     {
-        clientID: process.env.DISCORD_CLIENT_ID!,
-        clientSecret: process.env.DISCORD_CLIENT_SECRET!,
-        callbackURL: process.env.DISCORD_CALLBACK_URL,
-        scope: ['identify', 'email', 'guilds'],
-    }, 
-    async (accessToken: string, refreshToken: string, profile: Profile, done: VerifyCallback) => {
-        try{
-            const discordID = profile.id;
-            const existingUser = await db.user.findUnique(discordID);
-            
-            if (existingUser) {
-                console.log(`Existing User: ${existingUser.discordID}`);
-                await db.user.update(discordID, accessToken, refreshToken);
-                return done(null, existingUser);
-            }
+      clientID: process.env.DISCORD_CLIENT_ID!,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET!,
+      callbackURL: process.env.DISCORD_CALLBACK_URL,
+      scope: ["identify", "email", "guilds", "bot"],
+    },
+    async (
+      accessToken: string,
+      refreshToken: string,
+      profile: Profile,
+      done: VerifyCallback
+    ) => {
+      try {
+        const discordID = profile.id;
+        const existingUser = await db.user.findUnique(discordID);
 
-            const newUser = await db.user.create(discordID, accessToken, refreshToken);
-            return done(null, newUser);
-        } 
-        catch (err) {
-            console.log(err);
-            return done(err as any, undefined);
+        if (existingUser) {
+          console.log(`Existing User: ${existingUser.discordID}`);
+          await db.user.update(discordID, accessToken, refreshToken);
+          return done(null, existingUser);
         }
-    })
+
+        const newUser = await db.user.create(
+          discordID,
+          accessToken,
+          refreshToken
+        );
+        return done(null, newUser);
+      } catch (err) {
+        console.log(err);
+        return done(err as any, undefined);
+      }
+    }
+  )
 );
